4ca93f65643a7414577ead584206cd986fad87dd
raphaelmenges <unrealer2@yahoo.de>
2015-10-20 19:45:47 +0200

    Fixed bug.

--- src/Parser/LocalizationParser.cpp
+++ src/Parser/LocalizationParser.cpp
@@ -30,70 +30,72 @@ namespace eyegui
 
             // Create map
             std::unique_ptr<localizationMap> upMap = std::unique_ptr<localizationMap>(new localizationMap);
-		
+
             // Read file
-			std::ifstream in(filepath);
+            std::ifstream in(filepath);
 
             // Check whether file was found
             if (!in)
             {
                 throwError(OperationNotifier::Operation::PARSING, "Localization file not found", filepath);
             }
-		
-			// Some values for iteration
-			unsigned int line_count = 1;
-			std::string line;
-			
-			// Iterate through lines
-			while (getline(in, line)) 
-			{
-				// Check for invalid utf-8
-				std::string::iterator end_pos = utf8::find_invalid(line.begin(), line.end());
-				if (end_pos != line.end()) {
-					throwError(OperationNotifier::Operation::PARSING, "Invalid UTF-8 encoding detected at line " + line_count, filepath);
-				}
 
-				// Get rid of whitespaces
-				end_pos = std::remove(line.begin(), line.end(), ' ');
-				line.erase(end_pos, line.end());
+            // Some values for iteration
+            unsigned int line_count = 1;
+            std::string line;
 
-				// Get rid of tabs
-				end_pos = std::remove(line.begin(), line.end(), '\t');
-				line.erase(end_pos, line.end());
+            // Iterate through lines
+            while (getline(in, line))
+            {
+                // Check for invalid utf-8
+                std::string::iterator end_pos = utf8::find_invalid(line.begin(), line.end());
+                if (end_pos != line.end()) {
+                    throwError(OperationNotifier::Operation::PARSING, "Invalid UTF-8 encoding detected at line " + line_count, filepath);
+                }
 
-				// Extract key and value from line
-				parseLine(*(upMap.get()), line, filepath);
+                // Extract key and value from line
+                parseLine(*(upMap.get()), line, filepath);
 
-				// Next line
-				line_count++;
-			}
+                // Next line
+                line_count++;
+            }
 
-			// Return map for localization
-			return std::move(upMap);
+            // Return map for localization
+            return std::move(upMap);
         }
 
-		void parseLine(localizationMap& rLocalizationMap, std::string line, std::string filepath)
-		{
-			// Get left and right side
-			std::string delimiter = "=";
-			size_t pos = 0;
+        void parseLine(localizationMap& rLocalizationMap, std::string line, std::string filepath)
+        {
+            // Split left and right side
+            std::string delimiter = "=";
+            size_t pos = 0;
 
-			if ((pos = line.find(delimiter)) == std::string::npos)
-			{
-				throwError(OperationNotifier::Operation::PARSING, "Following line could not be parsed: " + line, filepath);
-				return;
-			}
+            if ((pos = line.find(delimiter)) == std::string::npos)
+            {
+                throwError(OperationNotifier::Operation::PARSING, "Following line could not be parsed: " + line, filepath);
+                return;
+            }
 
-			std::string left = line.substr(0, pos);
-			line.erase(0, pos + delimiter.length());
-			std::string right = line;
+            std::string left = line.substr(0, pos);
 
-			// Convert right side to utf-16
-			std::u16string utf16right;
-			utf8::utf8to16(right.begin(), right.end(), back_inserter(utf16right));
+            // Get rid of whitespaces on left side
+            std::string::iterator end_pos = std::remove(left.begin(), left.end(), ' ');
+            left.erase(end_pos, left.end());
 
-			// Fill in map
-			rLocalizationMap[left] = utf16right;
-		}
+            // Get rid of tabs on left side
+            end_pos = std::remove(left.begin(), left.end(), '\t');
+            left.erase(end_pos, left.end());
+
+            // Extract right side
+            line.erase(0, pos + delimiter.length());
+            std::string right = line;
+
+            // Convert right side to utf-16
+            std::u16string utf16right;
+            utf8::utf8to16(right.begin(), right.end(), back_inserter(utf16right));
+
+            // Fill in map
+            rLocalizationMap[left] = utf16right;
+        }
     }
 }
