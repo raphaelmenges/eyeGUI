8449ee7a78653685ebfac5c657fc69278bf09c85
raphaelmenges <unrealer2@yahoo.de>
2016-03-09 20:21:02 +0100

    Thoughts about dictionary.

--- src/Utilities/Dictionary.cpp
+++ src/Utilities/Dictionary.cpp
@@ -7,15 +7,39 @@
 
 #include "Dictionary.h"
 
+#include <fstream>
+
 // TODO: Delete
 #include <iostream>
 
 Dictionary::Dictionary()
 {
     std::cout << "Hallo welt" << std::endl;
+
+    // Read file with instream
+    std::ifstream in("/home/raphael/Downloads/german.dic");
+
+    if(in)
+    {
+        std::string line;
+        while (getline(in, line))
+        {
+            std::cout << line << std::endl;
+        }
+    }
 }
 
 Dictionary::~Dictionary()
 {
     // Nothing to do
+}
+
+bool Dictionary::checkForWord(std::u16string word) const
+{
+    return false;
+}
+
+std::vector<std::u16string> Dictionary::similarWords(std::u16string word, uint count) const
+{
+    return std::vector<std::u16string>();
 }
--- src/Utilities/Dictionary.h
+++ src/Utilities/Dictionary.h
@@ -9,6 +9,21 @@
 #ifndef DICTIONARY_H_
 #define DICTIONARY_H_
 
+#include "src/Defines.h"
+
+#include <string>
+#include <vector>
+
+// TODO:
+// - What datastructure? for each letter a node?
+// - should be able to match goofy and gofy (that is were it gets difficult)
+//  - Idea: duplicate all words with the same letters in one row, delete double
+//    / tripple letters and give a pointer from them to the "real word"
+// - Multiple dictionaries (URLs, Language....)
+// - internally std::string and functions which give back 16bit strings?
+
+// - Somehow a Trie structure, which collects all words. Problem: words like goofy which are gazed in as gofy in fast typing mode :(
+
 class Dictionary
 {
 public:
@@ -18,6 +33,13 @@ public:
 
     // Destructor
     virtual ~Dictionary();
+
+    // Check for exact word
+    bool checkForWord(std::u16string word) const;
+
+    // Give similar words
+    std::vector<std::u16string> similarWords(std::u16string word, uint count) const;
+
 
 private:
 };
