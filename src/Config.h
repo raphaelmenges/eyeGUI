7f98a29296897b6ac04baab6f8c7e6d2a96f2236
Raphael Menges <unrealer2@yahoo.de>
2016-11-29 17:47:46 +0100

    Defines for style value creation.

--- src/Config.h
+++ src/Config.h
@@ -9,10 +9,95 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+#define StyleValueMember(name, type, value) std::shared_ptr<StyleValue<type> > name = std::shared_ptr<StyleValue<type> >(new StyleValue<type>(value));
+#define StyleValueMemberMin(name, type, value, min) std::shared_ptr<StyleValue<type> > name = std::shared_ptr<StyleValue<type> >(new NumericStyleValue<type>(value, min));
+#define StyleValueMemberMinMax(name, type, value, min, max) std::shared_ptr<StyleValue<type> > name = std::shared_ptr<StyleValue<type> >(new NumericStyleValue<type>(value, min, max));
+
 #include "externals/GLM/glm/glm.hpp"
 
 namespace eyegui
 {
+	// General style value
+	template<typename T>
+	class StyleValue
+	{
+	public:
+
+		// Constructor takes initial value
+		StyleValue(T value) { mValue = value; }
+
+		// Setter for value
+		virtual void setValue(T value)
+		{
+			// Just store the value
+			mValue = value;
+		}
+
+		// Getter for value
+		T getValue() const { return mValue; }
+
+	protected:
+
+		// Protected default constructor called by subclass
+		StyleValue() {}
+
+		// Value
+		T mValue;
+	};
+
+	// Numeric style value (which can be clamped)
+	template<typename T>
+	class NumericStyleValue : public StyleValue<T>
+	{
+	public:
+
+		// Constructor takes initial value
+		NumericStyleValue(T value) : StyleValue<T>(value) {}
+		NumericStyleValue(T value, T min) { setMin(min); mValue = glm::max(value, min); }
+		NumericStyleValue(T value, T min, T max) { setMin(min); setMax(max); mValue = glm::clamp(value, min, max); }
+
+		// Setter for value. Includes clamping
+		virtual void setValue(T value) override
+		{
+			if (mMinimumSet && mMaximumSet)
+			{
+				mValue = glm::clamp(value, mMinimum, mMaximum);
+			}
+			else if (mMinimumSet)
+			{
+				mValue = glm::max(value, mMinimum);
+			}
+			else
+			{
+				mValue = value;
+			}
+		}
+
+	protected:
+
+		// Set minimum
+		void setMin(T min)
+		{
+			mMinimum = min;
+			mMinimumSet = true;
+		}
+
+		// Set maximum
+		void setMax(T max)
+		{
+			mMaximum = max;
+			mMaximumSet = true;
+		}
+
+		// Minimum
+		T mMinimum;
+		bool mMinimumSet = false;
+
+		// Maximum
+		T mMaximum;
+		bool mMaximumSet = false;
+	};
+
     struct Config
     {
         // Initialize with fallback values
@@ -22,7 +107,7 @@ namespace eyegui
         float sensorPenetrationDecreaseDuration = 1.5f;
         float buttonThresholdIncreaseDuration = 1.0f;
         float buttonThresholdDecreaseDuration = 2.0f;
-        float buttonPressingDuration = 0.3f;
+		float buttonPressingDuration = 0.3f;
         float sensorInteractionPenetrationAmount = 0.5f;
         float dimIncreaseDuration = 1.5f;
         float dimDecreaseDuration = 0.25f;
@@ -49,6 +134,10 @@ namespace eyegui
 		float futureKeyboardSpaceKeyThresholdMultiplier = 1.f;
 		float futureKeyboardBackspaceKeyThresholdMultiplier = 1.f;
 		float futureKeyboardSuggestionLineThresholdMultiplier = 1.f;
+
+		StyleValueMember(mspTest, float, 0.1f);
+		StyleValueMemberMin(mspTest2, float, 0.1f, 0.2f);
+		StyleValueMemberMinMax(mspTest3, float, 0.1f, 0.2f, 0.3f);
     };
 }
 
