29dd36a031411b378f5f4ddfc0858d5e5062e311
Raphael Menges <unrealer2@yahoo.de>
2017-06-16 11:31:46 +0200

    Testing OGG as sound format.

--- src/TestingArea.h
+++ src/TestingArea.h
@@ -10,11 +10,56 @@
 #define TESTING_AREA_H_
 
 #include "src/Style/StyleTree.h"
+#include "src/Utilities/PathBuilder.h"
+#include "externals/PortAudio/include/portaudio.h"
+#include "externals/stb/stb_vorbis.c"
 #include <iostream>
+#include <system_error>
 
 namespace eyegui
 {
-	int example(void);
+	short* buffer;
+	int bufferIndex = 0;
+	int samples = 0;
+
+
+	static int
+		output_cb(const void * inputBuffer, void * outputBuffer, unsigned long frames_per_buffer,
+			const PaStreamCallbackTimeInfo *time_info,
+			PaStreamCallbackFlags flags, void * data)
+	{
+		short *out = (short*)outputBuffer;
+		unsigned int i;
+		(void)inputBuffer; /* Prevent unused variable warnings. */
+		(void)time_info;
+		(void)flags;
+		(void)data;
+
+		for (i = 0; i<frames_per_buffer; i++)
+		{
+			*out++ = buffer[bufferIndex];
+			// TODO: channels
+			if (bufferIndex + 1 < samples)
+			{
+				bufferIndex++;
+			}
+			else
+			{
+				break;
+			}
+			
+
+			// TODO Do not overflow here. return paComplete instead
+		}
+		return paContinue;
+	}
+
+	static void
+		end_cb(void * data)
+	{
+		printf("end!\n");
+	}
+
 	void testingMain()
 	{
 		/*
@@ -40,241 +85,52 @@ namespace eyegui
 		std::cout << header->fetchProperty(StylePropertyFloat::AnimationDuration)->get() << std::endl;
 		std::cout << headerButton->fetchProperty(StylePropertyFloat::AnimationDuration)->get() << std::endl;
 		*/
+	
+		// EXPERIMENT WITH STB VORBIS
 
-		example();
-	}
+		std::string filepath = PathBuilder::buildFullFilepath("sounds/test.ogg");
+		int channels, sampleRate;
+		samples = stb_vorbis_decode_filename(filepath.c_str(), &channels, &sampleRate, &buffer);
+		std::cout << "Sample Rate: " << sampleRate << std::endl;
+		std::cout << "Channel Number: " << channels << std::endl;
+		std::cout << "Frame Number: " << samples << std::endl;
 
+		PaStreamParameters out_param;
+		PaStream * stream;
 
-	#include <stdio.h>
-	#include <math.h>
-	#include "portaudio.h"
+		// init portaudio
+		Pa_Initialize();
 
-	#define NUM_SECONDS   (1)
-	#define SAMPLE_RATE   (44100)
-	#define FRAMES_PER_BUFFER  (64)
-
-	#ifndef M_PI
-	#define M_PI  (3.14159265)
-	#endif
-
-	#define TABLE_SIZE   (200)
-
-	class Sine
-	{
-	public:
-		Sine() : stream(0), left_phase(0), right_phase(0)
+		out_param.device = Pa_GetDefaultOutputDevice();
+		if (out_param.device == paNoDevice)
 		{
-			/* initialise sinusoidal wavetable */
-			for (int i = 0; i<TABLE_SIZE; i++)
-			{
-				sine[i] = (float)sin(((double)i / (double)TABLE_SIZE) * M_PI * 2.);
-			}
-
-			sprintf(message, "No Message");
+			fprintf(stderr, "Have not found an audio device!\n");
 		}
 
-		bool open(PaDeviceIndex index)
-		{
-			PaStreamParameters outputParameters;
+		out_param.channelCount = channels;
+		out_param.sampleFormat = paInt16;
+		out_param.suggestedLatency = Pa_GetDeviceInfo(out_param.device)->defaultLowOutputLatency;
+		out_param.hostApiSpecificStreamInfo = NULL;
 
-			outputParameters.device = index;
-			if (outputParameters.device == paNoDevice) {
-				return false;
-			}
+		Pa_OpenStream(&stream, NULL, &out_param, sampleRate,
+			paFramesPerBufferUnspecified, paClipOff,
+			output_cb, NULL);
 
-			const PaDeviceInfo* pInfo = Pa_GetDeviceInfo(index);
-			if (pInfo != 0)
-			{
-				printf("Output device name: '%s'\r", pInfo->name);
-			}
+		Pa_SetStreamFinishedCallback(stream, &end_cb);
 
-			outputParameters.channelCount = 2;       /* stereo output */
-			outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
-			outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
-			outputParameters.hostApiSpecificStreamInfo = NULL;
+		Pa_StartStream(stream);
 
-			PaError err = Pa_OpenStream(
-				&stream,
-				NULL, /* no input */
-				&outputParameters,
-				SAMPLE_RATE,
-				paFramesPerBufferUnspecified,
-				paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-				&Sine::paCallback,
-				this            /* Using 'this' for userData so we can cast to Sine* in paCallback method */
-			);
+		printf("Play for 5 seconds.\n");
+		float duration = (float)samples / (float)(sampleRate * channels);
+		Pa_Sleep((long)(1000 * duration));
 
-			if (err != paNoError)
-			{
-				/* Failed to open stream to device !!! */
-				return false;
-			}
+		Pa_StopStream(stream);
 
-			err = Pa_SetStreamFinishedCallback(stream, &Sine::paStreamFinished);
+		Pa_CloseStream(stream);
 
-			if (err != paNoError)
-			{
-				Pa_CloseStream(stream);
-				stream = 0;
+		Pa_Terminate();
 
-				return false;
-			}
-
-			return true;
-		}
-
-		bool close()
-		{
-			if (stream == 0)
-				return false;
-
-			PaError err = Pa_CloseStream(stream);
-			stream = 0;
-
-			return (err == paNoError);
-		}
-
-
-		bool start()
-		{
-			if (stream == 0)
-				return false;
-
-			PaError err = Pa_StartStream(stream);
-
-			return (err == paNoError);
-		}
-
-		bool stop()
-		{
-			if (stream == 0)
-				return false;
-
-			PaError err = Pa_StopStream(stream);
-
-			return (err == paNoError);
-		}
-
-	private:
-		/* The instance callback, where we have access to every method/variable in object of class Sine */
-		int paCallbackMethod(const void *inputBuffer, void *outputBuffer,
-			unsigned long framesPerBuffer,
-			const PaStreamCallbackTimeInfo* timeInfo,
-			PaStreamCallbackFlags statusFlags)
-		{
-			float *out = (float*)outputBuffer;
-			unsigned long i;
-
-			(void)timeInfo; /* Prevent unused variable warnings. */
-			(void)statusFlags;
-			(void)inputBuffer;
-
-			for (i = 0; i<framesPerBuffer; i++)
-			{
-				*out++ = sine[left_phase];  /* left */
-				*out++ = sine[right_phase];  /* right */
-				left_phase += 1;
-				if (left_phase >= TABLE_SIZE) left_phase -= TABLE_SIZE;
-				right_phase += 3; /* higher pitch so we can distinguish left and right. */
-				if (right_phase >= TABLE_SIZE) right_phase -= TABLE_SIZE;
-			}
-
-			return paContinue;
-
-		}
-
-		/* This routine will be called by the PortAudio engine when audio is needed.
-		** It may called at interrupt level on some machines so don't do anything
-		** that could mess up the system like calling malloc() or free().
-		*/
-		static int paCallback(const void *inputBuffer, void *outputBuffer,
-			unsigned long framesPerBuffer,
-			const PaStreamCallbackTimeInfo* timeInfo,
-			PaStreamCallbackFlags statusFlags,
-			void *userData)
-		{
-			/* Here we cast userData to Sine* type so we can call the instance method paCallbackMethod, we can do that since
-			we called Pa_OpenStream with 'this' for userData */
-			return ((Sine*)userData)->paCallbackMethod(inputBuffer, outputBuffer,
-				framesPerBuffer,
-				timeInfo,
-				statusFlags);
-		}
-
-
-		void paStreamFinishedMethod()
-		{
-			printf("Stream Completed: %s\n", message);
-		}
-
-		/*
-		* This routine is called by portaudio when playback is done.
-		*/
-		static void paStreamFinished(void* userData)
-		{
-			return ((Sine*)userData)->paStreamFinishedMethod();
-		}
-
-		PaStream *stream;
-		float sine[TABLE_SIZE];
-		int left_phase;
-		int right_phase;
-		char message[20];
-	};
-
-	class ScopedPaHandler
-	{
-	public:
-		ScopedPaHandler()
-			: _result(Pa_Initialize())
-		{
-		}
-		~ScopedPaHandler()
-		{
-			if (_result == paNoError)
-			{
-				Pa_Terminate();
-			}
-		}
-
-		PaError result() const { return _result; }
-
-	private:
-		PaError _result;
-	};
-
-
-	/*******************************************************************/
-	int example(void)
-	{
-		Sine sine;
-
-		printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
-
-		ScopedPaHandler paInit;
-		if (paInit.result() != paNoError) goto error;
-
-		if (sine.open(Pa_GetDefaultOutputDevice()))
-		{
-			if (sine.start())
-			{
-				printf("Play for %d seconds.\n", NUM_SECONDS);
-				Pa_Sleep(NUM_SECONDS * 1000);
-
-				sine.stop();
-			}
-
-			sine.close();
-		}
-
-		printf("Test finished.\n");
-		return paNoError;
-
-	error:
-		fprintf(stderr, "An error occured while using the portaudio stream\n");
-		fprintf(stderr, "Error number: %d\n", paInit.result());
-		fprintf(stderr, "Error message: %s\n", Pa_GetErrorText(paInit.result()));
-		return 1;
+		delete buffer;
 	}
 }
 
